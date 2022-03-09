4be17fb1d8f44dbac3f840e2de65c45e1a9122f1
David Horstman <davidzilla27@gmail.com>
2022-03-09 16:57:23 -0500

    fix: fix update message

--- update.sh
+++ update.sh
@@ -2,4 +2,4 @@
 
 git remote add upstream git@github.com:ClipboardHealth/dotfiles.git
 git fetch upstream
-git merge upstream/main -m "Merge the latest changes from upstream ($(date +%Y-%m-%d))
+git merge upstream/main -m "Merge the latest changes from upstream ($(date +%Y-%m-%d))"
